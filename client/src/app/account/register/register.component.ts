40d1b90d61ee20b608b2e650cdde7b9814c1e068
VaradNaik <54986840+VaradNaik@users.noreply.github.com>
2024-04-30 21:43:17 -0400

    authentication with mysql implemented

--- client/src/app/account/register/register.component.ts
+++ client/src/app/account/register/register.component.ts
@@ -35,16 +35,93 @@
 //     this.registerUser();
 //   }
 // }
-import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
-import { Router } from '@angular/router';
+// import { Component } from '@angular/core';
+// import { FormGroup, FormControl, Validators } from '@angular/forms';
+// import { HttpClient } from '@angular/common/http';
+// import { Router } from '@angular/router';
 
+// @Component({
+//   selector: 'app-register',
+//   templateUrl: './register.component.html',
+//   styleUrls: ['./register.component.scss']
+// })
+// export class RegisterComponent {
+//   registerForm = new FormGroup({
+//     username: new FormControl('', [Validators.required, Validators.minLength(3)]),
+//     email: new FormControl('', [Validators.required, Validators.email]),
+//     password: new FormControl('', [Validators.required, Validators.minLength(6)])
+//   });
+
+//   constructor(private http: HttpClient, private router: Router) { }
+
+//   onSubmit(): void {
+//     this.http.post('http://localhost:8080/account/register', this.registerForm.value)
+//       .subscribe({
+//         next: (response) => {
+//           console.log('Registration successful', response);
+//           this.router.navigate(['/store']);  // Navigate to the store page
+//         },
+//         error: (error) => {
+//           console.error('Error during registration', error);
+//         }
+//       });
+//   }
+  
+// }
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { AccountService } from '../account.service';  // Ensure you import AccountService
+import { HttpErrorResponse } from '@angular/common/http';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
+// export class RegisterComponent {
+//   registerForm = new FormGroup({
+//     username: new FormControl('', [Validators.required, Validators.minLength(3)]),
+//     email: new FormControl('', [Validators.required, Validators.email]),
+//     password: new FormControl('', [Validators.required, Validators.minLength(6)])
+//   });
+
+//   constructor(
+//     private http: HttpClient, 
+//     private router: Router,
+//     private accountService: AccountService  // Inject the AccountService
+//   ) { }
+
+//   onSubmit(): void {
+//     this.http.post('http://localhost:8080/account/register', this.registerForm.value)
+//       .subscribe({
+//         next: (response) => {
+//           console.log('Registration successful', response);
+//           this.autoLogin(); // Call autoLogin after successful registration
+//         },
+//         error: (error) => {
+//           console.error('Error during registration', error);
+//         }
+//       });
+//   }
+
+//   private autoLogin() {
+//     const loginValues = {
+//       username: this.registerForm.value.username,
+//       password: this.registerForm.value.password
+//     };
+//     this.accountService.login(loginValues).subscribe({
+//       next: user => {
+//         const redirect = this.accountService.redirectUrl ? this.accountService.redirectUrl : '/store';
+//         this.router.navigateByUrl(redirect);
+//         this.accountService.redirectUrl = null; //clearing the redirect url post navigation
+//       },
+//       error: () => {
+//         console.error('Error Occurred during auto-login');
+//       }
+//     });
+//   }
+// }
 export class RegisterComponent {
   registerForm = new FormGroup({
     username: new FormControl('', [Validators.required, Validators.minLength(3)]),
@@ -52,21 +129,59 @@ export class RegisterComponent {
     password: new FormControl('', [Validators.required, Validators.minLength(6)])
   });
 
-  constructor(private http: HttpClient, private router: Router) { }
+  errorMessage: string = '';
+
+  constructor(
+    private http: HttpClient, 
+    private router: Router,
+    private accountService: AccountService // Include AccountService in the constructor
+  ) {}
 
   onSubmit(): void {
-    this.http.post('http://localhost:8080/account/register', this.registerForm.value)
-      .subscribe({
-        next: (response) => {
-          console.log('Registration successful', response);
-          this.router.navigate(['/store']);  // Navigate to the store page
-        },
-        error: (error) => {
-          console.error('Error during registration', error);
+    this.http.post('http://localhost:8080/account/register', this.registerForm.value).subscribe({
+      next: (response) => {
+        console.log('Registration successful', response);
+        this.autoLogin();
+      },
+      error: (error: HttpErrorResponse) => {
+        console.error('Error during registration', error);
+        if (error.status === 400) {
+          this.handleBadRequest(error.error);
+        } else {
+          this.errorMessage = 'An unexpected error occurred during registration.';
         }
-      });
+      }
+    });
   }
-  
+
+  private autoLogin() {
+    const loginValues = {
+      username: this.registerForm.value.username,
+      password: this.registerForm.value.password
+    };
+    this.accountService.login(loginValues).subscribe({
+      next: user => {
+        const redirect = this.accountService.redirectUrl ? this.accountService.redirectUrl : '/store';
+        this.router.navigateByUrl(redirect);
+        this.accountService.redirectUrl = null; // Clearing the redirect URL post navigation
+      },
+      error: () => {
+        console.error('Error occurred during auto-login');
+      }
+    });
+  }
+
+  private handleBadRequest(errorResponse: string) {
+    if (errorResponse === 'duplicate username') {
+      this.errorMessage = errorResponse;
+      // You can also set error directly on the form control
+      this.registerForm.controls['username'].setErrors({ 'duplicate': true });
+    } else if (errorResponse === 'duplicate emailId') {
+      this.errorMessage = errorResponse;
+      this.registerForm.controls['email'].setErrors({ 'duplicate': true });
+    }
+  }
 }
+
 
 
--- src/main/java/com/ecoomerce/sportscenter/controller/UserController.java
+++ src/main/java/com/ecoomerce/sportscenter/controller/UserController.java
@@ -2,9 +2,34 @@ package com.ecoomerce.sportscenter.controller;
 
 import com.ecoomerce.sportscenter.entity.UserEntity;
 import com.ecoomerce.sportscenter.repository.UserRepository;
+import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+
+//@RestController
+//@RequestMapping("/account")
+//public class UserController {
+//
+//    @Autowired
+//    private UserRepository userRepository;
+//
+//    @Autowired
+//    private PasswordEncoder passwordEncoder;
+//
+//    //@CrossOrigin(origins = "http://localhost:4200/account/register")
+//    @PostMapping("/register")
+//    public UserEntity registerUser(@RequestBody UserEntity userEntity) {
+//        UserEntity userEntity1 = new UserEntity();
+//        userEntity1.setUsername(userEntity.getUsername());
+//        userEntity1.setPassword(passwordEncoder.encode(userEntity.getPassword()));
+//        userEntity1.setEmail(userEntity.getEmail());
+//        return userRepository.save(userEntity1);
+//    }
+//}
 
 @RestController
 @RequestMapping("/account")
@@ -16,13 +41,28 @@ public class UserController {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
-    //@CrossOrigin(origins = "http://localhost:4200/account/register")
     @PostMapping("/register")
-    public UserEntity registerUser(@RequestBody UserEntity userEntity) {
-        UserEntity userEntity1 = new UserEntity();
-        userEntity1.setUsername(userEntity.getUsername());
-        userEntity1.setPassword(passwordEncoder.encode(userEntity.getPassword()));
-        userEntity1.setEmail(userEntity.getEmail());
-        return userRepository.save(userEntity1);
+    public ResponseEntity<?> registerUser(@RequestBody UserEntity userEntity) {
+        try {
+            UserEntity newUser = new UserEntity(userEntity.getUsername(), userEntity.getEmail(), passwordEncoder.encode(userEntity.getPassword()));
+            return ResponseEntity.ok(userRepository.save(newUser));
+        } catch (DataIntegrityViolationException e) {
+            if (e.getCause() instanceof ConstraintViolationException) {
+                ConstraintViolationException ex = (ConstraintViolationException) e.getCause();
+                String constraintName = ex.getConstraintName();
+                System.out.println(constraintName);
+                if ("users.username".equals(constraintName)) {
+                    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("duplicate username");
+                } else if ("users.email".equals(constraintName)) {
+                    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("duplicate emailId");
+                }
+            }
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred");
+        }
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred");
     }
 }
